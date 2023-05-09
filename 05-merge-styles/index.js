75d9a226e01b8909b63256a3f25abf6eae76b84f
Alexander <okyxxxf@gmail.com>
2023-05-09 15:00:35 +0300

    fix: fix bug with css bundle

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,20 +1,22 @@
 const fs = require('fs');
 const path = require('path');
-const styleList = [];
 const writeStream = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'bundle.css'));
 const reader = fs.promises.readdir(path.resolve(__dirname, 'styles'));
 
 reader.then((styles) => {
-  for(let style of styles){
-    if(style.slice(style.length - 4, style.length) === '.css'){
-      fs.promises.readFile(path.resolve(__dirname, 'styles', style)).then((data, err) => {
-        if (err) throw new Error(err);
-        styleList.push(data.toString());
-      }).then(() => {
-        for(let style of styleList){
-          writeStream.write(style);
+  fs.promises.readdir(path.resolve(__dirname, 'project-dist'))
+    .then((files) => {
+      if(files === 'bundle.css'){
+        fs.unlink(path.resolve(__dirname, 'project-dist', 'bundle.css'));
+      }
+    }).then(() => {
+      for(let style of styles){
+        if(style.slice(style.length - 4, style.length) === '.css'){
+          fs.promises.readFile(path.resolve(__dirname, 'styles', style)).then((data, err) => {
+            if (err) throw new Error(err);
+            writeStream.write(data.toString());
+          });
         }
-      });
-    }
-  }
+      }
+    });
 });
--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -34,23 +34,25 @@ const buildHtml = () => {
 };
 
 const mergeStyles = () => {
-  const styleList = [];
-  const writeStream = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'style.css'));
+  const writeStream = fs.createWriteStream(path.resolve(__dirname, 'project-dist', 'bundle.css'));
   const reader = fs.promises.readdir(path.resolve(__dirname, 'styles'));
 
   reader.then((styles) => {
-    for(let style of styles){
-      if(style.slice(style.length - 4, style.length) === '.css'){
-        fs.promises.readFile(path.resolve(__dirname, 'styles', style)).then((data, err) => {
-          if (err) throw new Error(err);
-          styleList.push(data.toString());
-        }).then(() => {
-          for(let style of styleList){
-            writeStream.write(style);
+    fs.promises.readdir(path.resolve(__dirname, 'project-dist'))
+      .then((files) => {
+        if(files === 'bundle.css'){
+          fs.unlink(path.resolve(__dirname, 'project-dist', 'bundle.css'));
+        }
+      }).then(() => {
+        for(let style of styles){
+          if(style.slice(style.length - 4, style.length) === '.css'){
+            fs.promises.readFile(path.resolve(__dirname, 'styles', style)).then((data, err) => {
+              if (err) throw new Error(err);
+              writeStream.write(data.toString());
+            });
           }
-        });
-      }
-    }
+        }
+      });
   });
 };
 
